c89521a5772493fbfee041e47dd2adfe019ba7ab
Diogo <braganca@stanford.edu>
2021-07-22 00:54:25 -0700

    Implemented Henry's modifications and J function

--- target/debug/main.d
+++ target/debug/main.d
@@ -1,1 +1,1 @@
-/home/diogo/work/ibps/target/debug/main: /home/diogo/work/ibps/src/main.rs build.rs
+/home/diogo/work/ibps/target/debug/main: /home/diogo/work/ibps/src/jfunc/ltrian.rs /home/diogo/work/ibps/src/jfunc.rs /home/diogo/work/ibps/src/main.rs build.rs
